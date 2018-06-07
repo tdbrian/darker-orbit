2823351742785f18c552b864aafb15768d13a45e
Thomas Brian <tbrian@appriver.com>
2018-06-06 19:21:23 -0500

    Update components and add boostrap

--- darker-orbit-webapp/pages/index.vue
+++ darker-orbit-webapp/pages/index.vue
@@ -1,30 +1,10 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        darker-orbit-webapp
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <b-container fluid>
+    <h1>TEST!!</h1>
+  </b-container>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
 export default {
   components: {
     AppLogo
